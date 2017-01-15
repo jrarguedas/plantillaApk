d402c2298175973990b4d2aefe9115c754939c33
jeankuj <jeankuj@gmail.com>
2017-01-15 14:55:53 -0600

    Se implemento la página de presentación

---
+++ PlantillaAPK/app/src/main/java/com/itcr/plantillaapk/PantallaPresentacion.java
@@ -0,0 +1,28 @@
+package com.itcr.plantillaapk;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PantallaPresentacion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_presentacion);
+        Thread myThread = new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(9000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
