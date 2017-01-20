e08d7e1cf05b4c3297d94613609a8428a1ef4ee7
jeankuj <jeankuj@gmail.com>
2017-01-20 09:59:43 -0600

    Se agregó una nueva funcionalidad a la notificación, y se elimino una imagen que no se necesitaba

--- PlantillaAPK/app/src/main/java/com/itcr/plantillaapk/MainActivity.java
+++ PlantillaAPK/app/src/main/java/com/itcr/plantillaapk/MainActivity.java
@@ -1,19 +1,12 @@
 package com.itcr.plantillaapk;
 
-
-import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.drawable.BitmapDrawable;
-import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.KeyEvent;
+
 
 import org.json.JSONException;
 
@@ -32,28 +25,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
-        Inicializador prueba = new Inicializador();
-        Radio radio = new Radio();
-
-        try {
-            String archivo = prueba.obtenerJson(this);
-            radio = prueba.obtenerDatos(archivo);
-
-            /*System.out.println("Nombre Radio : " + radio.getNombre());
-            System.out.println("Color APP: " + radio.getColor());
-            System.out.println("Pagina de la radio: " + radio.getUrlPagina());
-            System.out.println("URL del stream: " + radio.getStreamURL());
-            System.out.println("Punto de montaje: " + radio.getPuntoMontaje());
-            System.out.println("La descripcion: " + radio.getDescripcion());*/
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
 
         setContentView(R.layout.activity_main);
 
@@ -78,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
     public void onPause(){
         mNotificationManager = notificacion.notificacion(this);
         super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mNotificationManager != null){
+            notificacion.finalizarNotificacion(mNotificationManager);
+        }
+        super.onDestroy();
     }
 }
 
